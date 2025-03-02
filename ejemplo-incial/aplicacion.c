867671335b710054779aa6ddd31f70a7b9ad25ad
Javier <javier.moran02@uceva.edu.co>
2025-03-02 18:36:22 +0000

    conexion

---
+++ ejemplo-incial/aplicacion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Guardar con Ctrl O  enter, Salir con Ctrl X
+//Arreglo de String: char**
+//La funcion main es el entry point de un ejecutable de lenguaje c
+
+int main(){
+printf("\nHola mundo!\n");
+return 0;
+}
